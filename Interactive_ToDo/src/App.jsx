b8ed1b23fb551529cd52fcfaa95be577d572e57b
harmindersaini94 <harminder_saini94@outlook.com>
2024-08-13 23:56:22 -0700

    background

--- Interactive_ToDo/src/App.jsx
+++ Interactive_ToDo/src/App.jsx
@@ -3,11 +3,13 @@ import Home from './Components/Home'
 import ViewComponent from './Components/ViewComponent'
 import {Provider} from 'react-redux'
 import todoStore from './store'
+import Background from './Components/Background'
 
 function App() {
 
   return (
     <Provider store={todoStore}>
+      <Background />
         <Home />
         <ViewComponent />
     </Provider >
---
+++ Interactive_ToDo/src/Components/Background.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <div className='w-full h-full z-10 text-4xl text-white top-1/2 left-1/2 transform -transalte-x-1/2 -translate-y-1/2 bg-black'>INTERACTIVE TODO</div>
+  )
+}
+
+export default Background
